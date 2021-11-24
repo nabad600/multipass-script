531f07027e6fa3062cc69511abe5321ec6a1e3d0
Naba Das <nabakr.das@Nabas-Mac-mini.local>
2021-11-24 12:42:08 +0530

    modification

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ curl -L https://github.com/canonical/multipass/releases/download/v1.8.1/multipas
 # Install 
 sudo installer -pkg multipass-1.8.1+mac-Darwin.pkg -target /Applications
 # Create a virtual machine
-sleep 5
+sleep 10
 multipass launch --name deck-app
 multipass set client.primary-name=deck-app
 multipass exec deck-app -- bash -c "curl https://raw.githubusercontent.com/nabad600/multipass-script/main/multipass_install.sh | sh "
