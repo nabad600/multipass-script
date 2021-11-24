74c95b1b0b4e6311e52c15c2111840eb23bebd46
Naba Das <nabakr.das@Nabas-Mac-mini.local>
2021-11-24 13:45:39 +0530

    modification

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Package checking, Download and Install Multipass package
 pkgs='multipass'
-if [ sudo find /Applications -iname multipass.app != /Applications/Multipass.app ]! then
-  curl -L https://github.com/canonical/multipass/releases/download/v1.8.1/multipass-1.8.1+mac-Darwin.pkg -O --output-dir /tmp/
-  sudo installer -pkg multipass-1.8.1+mac-Darwin.pkg -target /Applications
+if [ sudo find /Applications -iname multipass.app == /Applications/Multipass.app ]! then
+    echo "Multipass install in your system"
+  else
+    curl -L https://github.com/canonical/multipass/releases/download/v1.8.1/multipass-1.8.1+mac-Darwin.pkg -O --output-dir /tmp/
+    sudo installer -pkg multipass-1.8.1+mac-Darwin.pkg -target /Applications
 fi
 #curl -L https://github.com/canonical/multipass/releases/download/v1.8.1/multipass-1.8.1+mac-Darwin.pkg -O --output-dir /tmp/
 # Install 
