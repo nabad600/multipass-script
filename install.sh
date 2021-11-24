ab002e66d018dc74e0d8961f9d3f5deda12efbc6
Naba Das <nabakr.das@Nabas-Mac-mini.local>
2021-11-24 14:53:41 +0530

    modification

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Package checking, Download and Install Multipass package
+blanko="";
 pkg=`which multipass`
-if [ "$pkg" == "0" ]; then
+if [ "$pkg" == "$blanko" ]; then
     echo "Multipass not install in your system"
     curl -L https://github.com/canonical/multipass/releases/download/v1.8.1/multipass-1.8.1+mac-Darwin.pkg -O --output-dir /tmp/
     sudo installer -pkg multipass-1.8.1+mac-Darwin.pkg -target /Applications
