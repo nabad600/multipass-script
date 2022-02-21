080edd105ddef651cb14405cfadb7f06574b6336
Naba Kumar Das <nabad600@gmail.com>
2022-02-21 22:35:59 +0530

    nfs add

--- multipass_install.sh
+++ multipass_install.sh
@@ -2,7 +2,7 @@
 #Set up the required package
 echo "Running apt update, installing dependencies"
 sudo apt update
-pkgs='curl uidmap apt-transport-https ca-certificates gnupg lsb-release docker.io neofetch'
+pkgs='curl uidmap apt-transport-https ca-certificates gnupg lsb-release docker.io neofetch autofs'
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
   sudo apt-get install -y $pkgs
 fi
@@ -14,10 +14,12 @@ echo "Staring docker";
 sudo loginctl enable-linger $(whoami)
 echo "sudo chmod 666 /var/run/docker.sock";
 sudo chmod 666 /var/run/docker.sock
-sudo wget https://raw.githubusercontent.com/nabad600/multipass-script/main/override.conf
+sudo wget https://raw.githubusercontent.com/deck-app/multipass-install/master/override.conf
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo cp override.conf /etc/systemd/system/docker.service.d/override.conf
 sudo rm -rf override.conf
+sudo touch /etc/auto.projects
+/home/${whoami}/Projects -fstype=nfs,rw,nosuid,proto=tcp,resvport ${hostname -I}:/Volumes/Disk\ 2/Projects/ >> /etc/auto.projects
 
 clear
 neofetch
